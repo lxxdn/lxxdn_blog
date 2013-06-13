3f8b64cecb1ab8d2f31e3d7f3daa35c2b9a4913e
lxxdn <julien.luxiao@gmail.com>
2013-06-13 22:31:36 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LxxdnBlog::Application.config.secret_token = '2551147ac2f6389fa23d87fb87b2d3e8001c9cbf703234694f52031f359f012f7526747944ce2a6ee54ff9a22fa6e02234096a48ad7a51993e63cd8d60679319'
