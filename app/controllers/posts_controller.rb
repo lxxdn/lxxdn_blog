1c9f867d371ef6605b693c45e3e4d15f79d79612
lxxdn <julien.luxiao@gmail.com>
2013-06-19 20:08:40 +0200

    add delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,4 +25,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
 end
