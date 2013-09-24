016f9f32131016765d2b43e7dd1ef607851aaaa9
lxxdn <julien.luxiao@gmail.com>
2013-09-25 00:23:10 +0200

    remove devise in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,4 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery
   
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(*User::ACCESSABLE_ATTRS) }
-  end
 end
